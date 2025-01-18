b266c177e1511c98eb330ddc0545b8ccafea306b
Beez131Github <olivercollins123@hotmail.com>
2025-01-19 11:06:06 +1300

    Datagen (broken)

---
+++ src/main/java/io/github/beez131github/grow_gc/datagen/ModModelProvider.java
@@ -0,0 +1,21 @@
+package io.github.beez131github.grow_gc.datagen;
+
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import io.github.beez131github.grow_gc.block.ModBlocks;
+import io.github.beez131github.grow_gc.block.custom.GoldenCarrotCropBlock;
+import net.minecraft.data.client.*;
+import net.minecraft.data.client.model.BlockStateModelGenerator;
+
+public class ModModelProvider extends FabricModelProvider {
+	public ModModelProvider(FabricDataOutput output) {
+		super(output);
+	}
+
+	@Override
+	public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+
+		blockStateModelGenerator.registerCrop(ModBlocks.GOLDEN_CARROT_CROP, GoldenCarrotCropBlock.AGE, 0, 1, 2, 3, 4, 5, 6);
+	}
+}
