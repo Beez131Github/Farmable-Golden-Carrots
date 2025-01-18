9730e32a509bbf40509750fc13e8919d7c09f092
Beez131Github <olivercollins123@hotmail.com>
2025-01-19 09:56:07 +1300

    Template adjust

---
+++ src/main/java/io/github/beez131github/grow_gc/grow_gc.java
@@ -0,0 +1,11 @@
+package io.github.beez131github.grow_gc;
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class grow_gc implements ModInitializer {
+
+	public void onInitialize() {
+		System.out.println("Growable golden carrots initialized!");
+	}
+}
