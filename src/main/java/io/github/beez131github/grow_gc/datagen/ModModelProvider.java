09e8b5a77bcbc5708b29b83f946cde65236343de
Beez131Github <olivercollins123@hotmail.com>
2025-01-19 18:29:22 +1300

    adjustments

--- src/main/java/io/github/beez131github/grow_gc/datagen/ModModelProvider.java
+++ src/main/java/io/github/beez131github/grow_gc/datagen/ModModelProvider.java
@@ -2,7 +2,7 @@ package io.github.beez131github.grow_gc.datagen;
 
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import io.github.beez131github.grow_gc.block.ModBlocks;
 import io.github.beez131github.grow_gc.block.custom.GoldenCarrotCropBlock;
 import net.minecraft.data.client.*;
@@ -17,5 +17,10 @@ public class ModModelProvider extends FabricModelProvider {
 	public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
 
 		blockStateModelGenerator.registerCrop(ModBlocks.GOLDEN_CARROT_CROP, GoldenCarrotCropBlock.AGE, 0, 1, 2, 3, 4, 5, 6);
+	}
+
+	@Override
+	public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+
 	}
 }
