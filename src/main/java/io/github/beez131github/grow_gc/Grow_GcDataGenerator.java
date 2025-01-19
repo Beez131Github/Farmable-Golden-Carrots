81baecde69c55050e0356669bfa975f61fe1702b
Beez131Github <olivercollins123@hotmail.com>
2025-01-19 22:14:12 +1300

    datagen attempt 2

---
+++ src/main/java/io/github/beez131github/grow_gc/Grow_GcDataGenerator.java
@@ -0,0 +1,15 @@
+package io.github.beez131github.grow_gc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import io.github.beez131github.grow_gc.datagen.*;
+
+public class Grow_GcDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+	}
+}
