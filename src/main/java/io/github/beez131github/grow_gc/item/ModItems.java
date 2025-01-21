79e812111e327c25021813ed2c0d0b3505ce42b0
Beez131Github <olivercollins123@hotmail.com>
2025-01-21 17:51:09 +1300

    i don't even know at this point

--- src/main/java/io/github/beez131github/grow_gc/item/ModItems.java
+++ src/main/java/io/github/beez131github/grow_gc/item/ModItems.java
@@ -1,39 +1,26 @@
 package io.github.beez131github.grow_gc.item;
 
-import io.github.beez131github.grow_gc.GrowGc;
-import net.minecraft.item.Item;
+import io.github.beez131github.grow_gc.GrowGc;;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
-
 public class ModItems {
-	// Register the item using the correct identifier
-	public static final Item ROOTED_GOLDEN_CARROT = registerItem(
-		"rooted_golden_carrot",
-		new Item(new Item.Settings())
-	);
 
-	private static Item registerItem(String name, Item item) {
-		Identifier id = Identifier.of(GrowGc.MOD_ID, name); // Create the identifier
-		RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id); // Create the registry key
+	// Rooted Golden Carrot Keys
+	public static final Item ROOTED_GOLDEN_CARROT = registerItem("rooted_golden_carrot", new Item(new Item.Settings()
+		.registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(GrowGc.MOD_ID, "rooted_golden_carrot")))
+		.maxCount(1)
+	));
 
-		// Update the Item.Settings to include the registry key
-		Item.Settings settings = new Item.Settings()
-			.registryKey(key); // Explicitly set the registry key in the settings
-
-		// Register the item using the registry key
-		return Registry.register(
-			Registries.ITEM, // The registry to register to
-			key,             // The registry key
-			new Item(settings) // Create the item with updated settings
-		);
+	public static Item registerItem(String name, Item item) {
+		return Registry.register(Registries.ITEM, Identifier.of(GrowGc.MOD_ID, name), item);
 	}
 
-
-	// Method to register all mod items
+	// Method to add item to item group
 	public static void registerModItems() {
 		GrowGc.LOGGER.info("Registering Mod Items for " + GrowGc.MOD_ID);
 	}
